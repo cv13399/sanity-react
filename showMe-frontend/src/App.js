3ad8d726e24444122ea8f4a1fe26778dc13bfdf0
Chase Lee <chase@study.cat>
2023-05-21 02:00:09 +0800

    today's work

--- showMe-frontend/src/App.js
+++ showMe-frontend/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
+import {Routes, Route, useNavigate} from "react-router-dom";
 
 const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <Route>
+        </Route>
     )
 }
 
