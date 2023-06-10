bc5c1106a101b5625265c71ab1ebfee0c6eb5fbf
Chase Lee <chase@study.cat>
2023-06-11 02:01:27 +0800

    Google login fixed

--- showMe-frontend/src/components/Login.jsx
+++ showMe-frontend/src/components/Login.jsx
@@ -1,26 +1,55 @@
 import React from 'react';
-import GoogleLogin from 'react-google-login';
+import {GoogleLogin, GoogleOAuthProvider, googleLogout} from '@react-oauth/google';
 import {useNavigate} from 'react-router-dom';
 import {FcGoogle} from 'react-icons/fc'; 
 import shareVideo from '../assets/share.mp4';
-import logo from '../assets/logo.png';
+import logo from '../assets/logowhite.png';
+
 
 const Login = () => {
-  return (
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
 
+  return (
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
     <div className="flex justify-start items-center flex-col h-screen">
       <div className="relative w-full h-full">
         <video 
           src={shareVideo}
           type="video/mp4"
-          Loop
+          loop
           controls={false}
           muted
           autoPlay
           className='w-full h-full object-cover'
         />
       </div>
+      <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+        <div className="p-5">
+          <img src={logo} width="130px" alt="logo" />
+        </div>
+        <div className="shadow-2xl">
+          <GoogleLogin 
+            render={(renderProps)=> (
+              <button
+                type="button"
+                className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+              >
+                <FcGoogle className='mr-4'/>
+                  Sign in with Google
+              </button>
+            )}
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            // cookiePolicy='single_host_origin'
+          />
+        </div>
+      </div>
     </div>
+    </GoogleOAuthProvider>
   )
 }
 
