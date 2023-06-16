63a1c64200b881c2c06b60c291f4b00883fc6ba1
Chase Lee <chase@study.cat>
2023-06-16 13:05:17 +0800

    added document title

--- showMe-frontend/src/App.js
+++ showMe-frontend/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useEffect}  from 'react';
 import {Routes, Route, useNavigate} from "react-router-dom";
 
 import Login from './components/Login';
 import Home from './container/Home';
 
 const App = () => {
+    useEffect(() => {
+        // Update the document title using the browser API
+        document.title = "寶理三官廟";
+    });
+    
     return (
         <Routes>
             <Route path='login' element={<Login />} />
